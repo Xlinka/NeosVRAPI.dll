559cd0168ce37cc89ec3f575cc8fd61f2bca95b7
xLinka <Oliver.dunnington@outlook.com>
2023-08-31 14:14:07 +0100

    Start

---
+++ NEOSVRAPI.dll/NEOSVRAPI.dll/ApiClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace NeosApiLibrary
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://api.neos.com"; // Adjust the actual API base URL
+
+        public ApiClient(string accessToken)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            return _httpClient;
+        }
+    }
+}

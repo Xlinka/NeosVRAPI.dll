78536cfa398e728e154f46cae060747a2b30dbda
xLinka <Oliver.dunnington@outlook.com>
2023-08-31 14:29:23 +0100

    bwah

---
+++ NEOSVRAPI.dll/NEOSVRAPI.dll/UserSessions/login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NeosApiLibrary.UserSessions
+{
+    public class UserSessionsLogin
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserSessionsLogin(ApiClient apiClient)
+        {
+            _httpClient = apiClient.GetHttpClient();
+        }
+
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            try
+            {
+                var loginData = new
+                {
+                    username,
+                    password
+                };
+
+                var json = JsonSerializer.Serialize(loginData);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync($"{ApiClient.BaseUrl}/api/userSessions", content);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new NeosApiException("Error while logging in.", ex);
+            }
+        }
+    }
+}
---
+++ NEOSVRAPI.dll/NEOSVRAPI.dll/Users/GetUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NeosApiLibrary.Users
+{
+    public class GetUser
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetUser(ApiClient apiClient)
+        {
+            _httpClient = apiClient.GetHttpClient();
+        }
+
+        public async Task<string> GetUserInfoAsync(string userId)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{ApiClient.BaseUrl}/api/users/{userId}");
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new NeosApiException($"Error while getting user info for user ID {userId}.", ex);
+            }
+        }
+    }
+}
